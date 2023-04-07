e7f52a125a39a407af950dfd82daacdcd3202317
nnmm <nnmmgit@gmail.com>
2023-04-07 13:06:32 +0200

    WIP in progress

--- rclrs/src/parameter/node_parameters.rs
+++ rclrs/src/parameter/node_parameters.rs
@@ -6,10 +6,19 @@ use super::{ParameterOverrideMap, ParameterValue};
 pub(crate) enum Parameter {
     ReadOnly(ParameterValue),
     ReadWrite(ParameterValue),
+    // Uninitialized? https://github.com/ros2/rclcpp/pull/1673
+}
+
+
+
+
+pub struct ConstrainedParameter {
+
 }
 
 pub enum ParameterError {
-
+    TypeMismatch,
+    InvalidName,
 }
 
 // RCLCPP_PUBLIC
@@ -22,16 +31,41 @@ pub enum ParameterError {
 // rcl_interfaces::msg::ParameterDescriptor(),
 // bool ignore_override = false) = 0;
 
+It is a read-only parameter if 
+
+// Parameter states:
+// - Undeclared (although there might be a "override"/"initial" value for it )
+// - 
+
+
+
+// What happens when you declare a parameter again?
+// => Exception
 
 // What when a set_parameter service call arrives before a parameter is declared? => If allow_undeclared_parameters
 // Do we want to replicate the behavior of get_parameter() to return an empty rclcpp::Parameter{name} if allow_undeclared?
 // Would it be fair to say that the parameters = a map with defaults for some values (= overrides)?
 // => How would the service handle this?
 
+
+/// The declare() function is responsible for specifying the intended type of the parameter
+
+/// The node can receive parameters from the "outside", i.e. given
+/// directly on the command line or in yaml files, in the node builder,
+/// or via parameter services.
+/// These parameters can then be validated by the node.
 pub struct NodeParameters {
+    // This is entirely immutable.
     overrides: ParameterOverrideMap,
-    parameters: BTreeMap<String, Parameter>,
-    allow_undeclared: bool,
+    parameters: BTreeMap<String, Parameter>
+    allow_set_parameters_service_to_declare_parameters: bool,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub(crate) enum ParameterEntry<T> {
+    InitialValue(T),
+    ReadOnly(T),
+    ReadWrite(T),
 }
 
 impl NodeParameters {
@@ -42,11 +76,20 @@ impl NodeParameters {
         }
     }
 
-    pub fn ro_parameter(&self, name: &str) -> Option<&ParameterValue> {
-        // Uhh, TODO
+    // pub fn get_unused_overrides()
+
+    pub fn get(&self, name: &str) -> Option<&ParameterValue> {
         match self.parameters.get(name)? {
+            Parameter::InitialValue(_) => None,
             Parameter::ReadOnly(pv) => Some(pv),
             Parameter::ReadWrite(pv) => Some(pv),
         }
     }
+
+    pub fn entry<T>(&self, name: &str) -> Option<ParameterEntry<T>> {
+        let param = self.parameters.get_mut(name)?;
+
+    }
+
+    // declare = entry::<String>("foo")?;
 }
